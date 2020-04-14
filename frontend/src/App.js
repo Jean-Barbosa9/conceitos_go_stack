8523f60c485b7125eb9490c706cf1effe0061c24
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-04-14 00:56:43 -0300

    Aplicando conceitos de estado e imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Header from './components/Header';
 
 export default function App() {
+  const [projects, setProjects] = useState([
+    'Aplicação Web Front-End',
+    'Aplicativo Mobile',
+  ])
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
   return (
     <Fragment>
-      <Header title="GoStack 11 Rocketseat">
-        <ul>
-          <li>Node com banco SQL</li>
-          <li>ReactJS</li>
-          <li>React Native</li>
-        </ul>
-      </Header>
-      <Header title="React Front to Back Udemy">
-        <ul>
-          <li>ReactJS com Context API</li>
-          <li>ReactJS com Redux</li>
-          <li>Integração com Firestore</li>
-        </ul>
-      </Header>
+      <Header title="GoStack 11 Rocketseat" />
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+      <button type="button" 
+        onClick={handleAddProject}>Adicionar projeto</button>
     </Fragment>
   )
 }
