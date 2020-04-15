cb7f236a04f0fe190f096ede4e4689d0857f8dc1
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-04-14 23:35:10 -0300

    Importação de CSS e de imagem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react';
-
+import './App.css';
 import Header from './components/Header';
+import sampleImg from'./assets/sample-image.jpg';
 
 export default function App() {
   const [projects, setProjects] = useState([
@@ -15,6 +16,7 @@ export default function App() {
   return (
     <Fragment>
       <Header title="GoStack 11 Rocketseat" />
+      <img width={300} src={sampleImg} alt="Imagem de exemplo para importação"/>
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
       </ul>
